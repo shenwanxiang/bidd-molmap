91e1e3588f912fea276398eb6571b534022cf5af
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2020-01-02 15:48:15 +0800

    sampling images for lim x range

--- molmap/data-processing/pubchem/04_sampling.py
+++ molmap/data-processing/pubchem/04_sampling.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-import pqkmeans
 from joblib import Parallel, delayed
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 from tqdm import tqdm
 from itertools import chain
 import seaborn as sns
@@ -55,7 +55,7 @@ def sample(tsn, bins_2_smaple = 100, tag = 'sample'):
 if __name__ == '__main__':
     
     
-    
+
     bitsize = 2048
     total_sample = 96117900
     bins_2_smaple = 100
@@ -64,25 +64,41 @@ if __name__ == '__main__':
     ## sample
     df_sample = sample(tsn_all, tag = 'sample')
 
-    
     # save distribute figures
     fig, (ax1, ax2) = plt.subplots(2, sharex=True,  figsize=(12,10), gridspec_kw={'hspace': 0.05})
 
     alldata = df_sample['NumOnbits']
     ax1 = alldata.hist(bins = 100, edgecolor = 'black', ax = ax1, ylabelsize = 14, color = 'lightblue')
-    ax1.text(410, 10000000, 'Overall: %s' % len(alldata), fontsize=16)
-    ax1.set_ylabel('Number of compunds', fontsize=14)
+
 
     sampled = df_sample[df_sample['sample'] == True]['NumOnbits']
     ax2 = sampled.hist(bins = 100, edgecolor = 'black', ax = ax2, ylabelsize = 14,  color = 'lightblue')
-    ax2.text(410, 1280000, 'Sample: %s' % len(sampled), fontsize=16)
-    ax2.set_ylabel('Number of compunds', fontsize=14)
-    fig.savefig('./data/sample_result.pdf')
-    
+
+
+    def millions(x, pos):
+        'The two args are the value and tick position'
+        return '%1.1fM' % (x * 1e-6)
+
+
+    formatter = FuncFormatter(millions)
+    ax1.yaxis.set_major_formatter(formatter)
+    ax2.yaxis.set_major_formatter(formatter)
+
+    ax1.set_ylabel('Number of Compunds', fontsize=14)
+    ax2.set_ylabel('Number of Compunds', fontsize=14)
+    ax2.set_xlabel('Number of On Bits', fontsize=14)
+
+
+    ax1.text(410/3, 10000000, 'Overall: %s' % len(alldata), fontsize=16)
+    ax2.text(410/3, 1280000, 'Sample: %s' % len(sampled), fontsize=16)
+    plt.xlim(0,250)
+
+    fig.savefig('./data/sample_result.png', dpi = 320)
+
     # save sampled cid & smiles
     sidx = sampled.index
     dfsmiles = pd.read_csv('./data/cid.smiles.nodupstring.96117900.csv', index_col = 0)
-    
+
     s = dfsmiles.iloc[sidx]
     s.index.name = 'index'
     s.to_csv('./data/cid_smiles.sample.csv')

8c6d3f64f628950d2c70f009858ddb606614f96b
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2020-01-09 21:36:02 +0800

    add example

--- paper/05_speed_comparison/run_kekulescope.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-import molmap
-from molmap import dataset
-path = os.path.join(os.path.dirname(os.path.dirname(molmap.__file__)), 
-                    'paper/03_KekuleScope_comparison/kekulescope')
-sys.path.insert(0, path)

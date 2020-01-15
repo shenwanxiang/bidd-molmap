35b2cc48462a566e4541b19504e95a551b348ac6
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2020-01-15 14:58:10 +0800

    update chembl pcba script

--- paper/02_OutofTheBox_benchmark_comparison_DMPNN/special_config.py
+++ paper/02_OutofTheBox_benchmark_comparison_DMPNN/special_config.py
@@ -1,9 +1,9 @@
 # the dense layers for these multi outputs tasks
 
 dense_layers_config = {
-    'ChEMBL': [2048], # 1310 outputs
+    'ChEMBL': [1563], # 1310 outputs
     'ToxCast': [1024], # 617 outputs
-    'PCBA': [512], #128 outputs
+    'PCBA': [256], #128 outputs
     'MUV':[512], #17 outputs
     'SIDER': [128], # 27 outputs
     'Tox21':[256], # 12  outputs}

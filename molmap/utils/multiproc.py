9baced0fbf7d2eee8819c9545071ca94c29b0714
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2020-03-22 21:28:38 +0800

    fix import issue

--- molmap/utils/multiproc.py
+++ molmap/utils/multiproc.py
@@ -17,7 +17,7 @@ from multiprocessing import Pool,cpu_count,current_process
 import subprocess
 
 
-from molmap.logtools import print_info, print_error, pbar,print_warn
+from molmap.utils.logtools import print_info, print_error, pbar,print_warn
 
 
 def RunCmd(cmd):

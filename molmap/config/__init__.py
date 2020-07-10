7a9277c5748adc29b3339c46e3f68ad787cfc98e
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2020-07-10 12:34:47 +0800

    mega download support

--- molmap/config/__init__.py
+++ molmap/config/__init__.py
@@ -1,11 +1,27 @@
 from molmap.utils.logtools import print_info
 import gdown
+from mega import Mega
 import pandas as pd
 import os
 
-fids = {'fingerprint_correlation.cfg.gzip': '1E0QRu8e0_bqqnrC894GcbJHgfLR57QEo',
-        'fingerprint_cosine.cfg.gzip': '1dHgpol9K7Wo72rs7g0UrToGTUaouoT8a', 
-        'fingerprint_jaccard.cfg.gzip': '1EMYbw3wGguX-KSWjRGVhYZdwMdmisb2p'}
+#!pip install mega.py==1.0.8
+
+# download from google drive
+googleids = {'fingerprint_correlation.cfg.gzip': 'https://drive.google.com/uc?id=1E0QRu8e0_bqqnrC894GcbJHgfLR57QEo',
+             'fingerprint_cosine.cfg.gzip': 'https://drive.google.com/uc?id=1dHgpol9K7Wo72rs7g0UrToGTUaouoT8a', 
+             'fingerprint_jaccard.cfg.gzip': 'https://drive.google.com/uc?id=1EMYbw3wGguX-KSWjRGVhYZdwMdmisb2p'}
+
+
+
+
+# download from mega drive
+megaids = {'fingerprint_correlation.cfg.gzip': 'https://mega.nz/file/3uBziIzQ#QfvdlG1CRbuO-rADH6eqlJ361caqkvzq1_gyDDtLYmc',
+           'fingerprint_cosine.cfg.gzip': 'https://mega.nz/file/S6IR0I4K#nk-vn3wm8makWoqg0VoNCcPSKpPxx_oq14JhW1hJHwg', 
+           'fingerprint_jaccard.cfg.gzip': 'https://mega.nz/file/L7QTFIyQ#bnlWIcuAUBUB0EUmb8Y29vY3ExGNGTJTZeU-nl3OeM0'}
+
+
+
+
 
 
 
@@ -19,12 +35,23 @@ def load_config(ftype = 'descriptor', metric = 'cosine'):
     if os.path.exists(filename):
         df = pd.read_pickle(filename, compression = 'gzip')
     else:
-        print_info('downloading config file from google drive...')
+        
         name = '%s_%s.cfg.gzip' % (ftype, metric)
         filename = os.path.join(dirf, name)
-        fid = fids.get(name)
-        url = "https://drive.google.com/uc?id=%s" % fid
-        filename = gdown.download(url, filename, quiet = False)
-        print_info('finished...')
+        
+        try:
+            url = googleids.get(name)        
+            print_info('downloading config file from google drive: %s' % url)
+            filename = gdown.download(url, filename, quiet = False)
+            print_info('finished...')
+            
+        except:
+            url = megaids.get(name) 
+            print_info('downloading config file from Mega drive: %s' % url)
+            
+            mega  = Mega()
+            mega.download_url(url, dest_filename = filename)
+            print_info('finished...')
+
         df = pd.read_pickle(filename,compression = 'gzip')
     return df

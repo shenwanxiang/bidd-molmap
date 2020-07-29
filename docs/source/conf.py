2499cb5adc0c1a91f30208c9896527392fdd84de
Charleshen <senwanxiang@163.com>
2020-07-29 15:01:46 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@
 
 
 # -- Project information -----------------------------------------------------
+master_doc = 'index'
 
 project = 'molmap'
 copyright = '2020, Shen Wan Xiang'
@@ -47,10 +48,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_theme = "sphinx_rtd_theme"
 #html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,

10a8b66209ccc3a38165d58b4eb5975d9fe903e7
shenwanxiang <--global>
2020-07-29 14:38:02 +0800

    change conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,9 +47,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 #html_theme = 'alabaster'
 
